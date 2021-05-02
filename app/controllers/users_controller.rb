8d8f875d20e1d219b6ad58037e3f1e7d4dcfbdd6
Northrain-cmd <northrain@icloud.com>
2021-05-02 15:03:17 +0300

    Assignment complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,16 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
-    
+    @user = User.find(params[:id])
+    if @user.update user_params
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
   end
 
   private
